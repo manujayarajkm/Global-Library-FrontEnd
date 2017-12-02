7ae6e7b613a0b822490b41f1a2b9399988a9e22c
Manu Jayaraj <manujayarajkm@gmail.com>
2017-12-02 14:24:13 +0530

    added no of seats restriction

--- src/app/bootstrap-test/bootstrap-test.component.ts
+++ src/app/bootstrap-test/bootstrap-test.component.ts
@@ -14,17 +14,39 @@ export class BootstrapTestComponent implements OnInit {
   seat4:String="vaccant.png";
   seat5:String="vaccant.png";
   expr1:boolean;
+  seatvar:String;
   seat:string[]=[];
   constructor() { }
 
   toggleimage(id){
+    if(this.seat.length>=2){
+      console.log('first element of the array '+this.seat[0]);
+      if(this.seat[0]=='seat2'){
+        this.seat2="vaccant.png";
+      }else if(this.seat[0]=='seat1'){
+        this.seat1="vaccant.png";
+      }
+      else if(this.seat[0]=='seat3'){
+        this.seat3="vaccant.png";
+      }
+      else if(this.seat[0]=='seat4'){
+        this.seat4="vaccant.png";
+      }
+      else if(this.seat[0]=='seat5'){
+        this.seat5="vaccant.png";
+      }
+      console.log('seatvar '+this.seatvar);
+      this.splicearray(this.seat[0]);
+      console.log('no exceeded '+this.seat);
+    }
+    
     console.log(id);
     
-      if(id=='s1')
+      if(id=='seat1')
       {
       if(this.seat1=='vaccant.png'){
       this.seat1="selected.jpeg";
-      var length=this.seat.push('s1');
+      var length=this.seat.push('seat1');
       console.log('after insertion '+this.seat);
       }
       else if(this.seat1=='selected.jpeg'){
@@ -34,67 +56,68 @@ export class BootstrapTestComponent implements OnInit {
       // if (index > -1) {
       //    this.seat.splice(index, 1);
       // }
-      this.splicearray('s1');
+      this.splicearray('seat1');
         console.log('after removal '+this.seat);
       }
     }
       
-      else if(id=='s2')
+      else if(id=='seat2')
       {
       if(this.seat2=='vaccant.png'){
       this.seat2="selected.jpeg";
-      var length=this.seat.push('s2');
+      var length=this.seat.push('seat2');
       console.log('after insertion '+this.seat);
       }
       else if(this.seat2=='selected.jpeg'){
         this.seat2='vaccant.png';
         //this.seat.pop();
-        this.splicearray('s2');
+        this.splicearray('seat2');
         console.log('after removal '+this.seat);
       }
     }
-    else if(id=='s3')
+    else if(id=='seat3')
     {
     if(this.seat3=='vaccant.png'){
     this.seat3="selected.jpeg";
-    var length=this.seat.push('s3');
+    var length=this.seat.push('seat3');
     console.log('after insertion '+this.seat);
     }
     else if(this.seat3=='selected.jpeg'){
       this.seat3='vaccant.png';
       //this.seat.pop();
-      this.splicearray('s3');
+      this.splicearray('seat3');
       console.log('after removal '+this.seat);
     }
   }
-  else if(id=='s4')
+  else if(id=='seat4')
   {
   if(this.seat4=='vaccant.png'){
   this.seat4="selected.jpeg";
-  var length=this.seat.push('s4');
+  var length=this.seat.push('seat4');
   console.log('after insertion '+this.seat);
   }
   else if(this.seat4=='selected.jpeg'){
     this.seat4='vaccant.png';
     //this.seat.pop();
-    this.splicearray('s4');
+    this.splicearray('seat4');
     console.log('after removal '+this.seat);
   }
 }
-else if(id=='s5')
+else if(id=='seat5')
 {
 if(this.seat5=='vaccant.png'){
 this.seat5="selected.jpeg";
-var length=this.seat.push('s5');
+var length=this.seat.push('seat5');
 console.log('after insertion '+this.seat);
 }
 else if(this.seat5=='selected.jpeg'){
   this.seat2='vaccant.png';
   //this.seat.pop();
-  this.splicearray('s5');
+  this.splicearray('seat5');
   console.log('after removal '+this.seat);
 }
 }
+    
     
   }
 submit(){
